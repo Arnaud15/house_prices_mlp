f832c1a23da129d68437ec050b1cc1583e67c6a0
Arnaud15 <arnaud.autef@gmail.com>
2021-09-10 09:18:06 -0700

    preprocessing written not fully tested

--- house_prices.py
+++ house_prices.py
@@ -1,24 +1,81 @@
-from typing import List, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 
 import numpy as np
 import pandas as pd
 
 
-def embed_column(col: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    """Turns a numpy array into an integer array corresponding to its unique values
+def embed_column(
+    col: np.ndarray, add_unkown_token: bool
+) -> Tuple[np.ndarray, Dict]:
+    """Turns a numpy array into an integer array corresponding to its unique
+    values.
     Args:
         col: the numpy array to encode
+        add_unkown_token: True if an additional integer embedding is added.
     Returns:
         (encoded_col, mapping)
-        encoded_col has shape col.shape = the integer codes corresponding to each value in the input array
-        mapping has shape (n_uniques,) and values mapping[i] = value corresponding to code i
+        encoded_col has shape col.shape = the integer codes corresponding to
+        each value in the input array
+        mapping is a dictionary with n_uniques keys. keys are observed values
+        in the column, their corresponding value is their embedding indice.
     """
     assert len(col.shape) == 1, "only embedding 1D arrays"
     _, indices, encoded_col = np.unique(
         col, return_index=True, return_inverse=True, return_counts=False
     )
-    return encoded_col, col[indices]
+    n_uniques = indices.shape[0]
+    mapping = {col[indice]: idx for (idx, indice) in enumerate(indices)}
+    if add_unkown_token:
+        mapping["UNK_TOKEN"] = n_uniques
+    return (
+        encoded_col,
+        mapping,
+    )
+
+
+def scale_min_max(val, min_val, max_val):
+    """Dummy utility to scale values between -1 and 1"""
+    return 2 * (val - min_val) / (max_val - min_val) - 1
+
+
+def scale_column(
+    col: np.ndarray,
+) -> Tuple[np.ndarray, Optional[Tuple[float, float]]]:
+    """Scale entries in the input to the range [-1, 1].
+    Args:
+        col: the ndarray to scale
+    Returns:
+        scaled: the scaled output
+        min_max: the min and the max values observed and used for scaling
+    """
+    assert len(col.shape) == 1
+    assert not np.any(np.isnan(col)), "NaN values in array to rescale"
+    min_, max_ = col.min(), col.max()
+    if (max_ - min_) < 1e-12:
+        return np.zeros_like(col), None
+    return scale_min_max(col, min_, max_), (min_, max_)
+
+
+def log_normalize(col: np.ndarray) -> np.ndarray:
+    """Normalize inputs using a log"""
+    assert col.min() > 0.0
+    return np.log(col)
+
+
+def flatten_preprocessed(
+    transformed_cols: Dict[str, np.ndarray], info: Dict[str, Any]
+) -> Tuple[np.ndarray, List[Any]]:
+    """Utility to flatten a dictionary of transformed columns to a matrix of
+    training examples, and their associated dictionary of preprocessing info to a list."""
+    assert len(transformed_cols) == len(info)
+    all_cols = sorted(list(transformed_cols.keys()))
+    design_matrix = np.concatenate(
+        [transformed_cols[col].reshape(-1, 1) for col in all_cols], axis=1
+    )
+    assert len(design_matrix.shape) == 2
+    assert design_matrix.shape[1] == len(all_cols)
+    return design_matrix, [info[col] for col in all_cols]
 
 
 def preprocess_data(
@@ -26,9 +83,33 @@ def preprocess_data(
     to_embed: List[str],
     to_scale: List[str],
     to_lognorm: List[str],
-    to_drop: List[str],
-) -> pd.DataFrame:
+    to_identity: List[str],
+) -> Tuple[np.ndarray, List[Any]]:
     assert "SalePrice" in data.columns, "incomplete frame"
-    data = data.drop(columns=to_drop)
-    return data
+    transformed = {}
+    preprocess_info = {}
+
+    for col in to_embed:
+        embedded, mapping = embed_column(
+            data.loc[:, col].values, add_unkown_token=True
+        )
+        transformed[col] = embedded
+        preprocess_info[col] = mapping
+
+    for col in to_scale:
+        scaled, min_max = scale_column(data.loc[:, col].values)
+        transformed[col] = scaled
+        preprocess_info[col] = min_max
+
+    for col in to_lognorm:
+        transformed[col] = log_normalize(data.loc[:, col].values)
+        preprocess_info[col] = None
+
+    for col in to_identity:
+        transformed[col] = data.loc[:, col].values
+        preprocess_info[col] = None
+
+    design_matrix, info = flatten_preprocessed(transformed, preprocess_info)
+    assert design_matrix.shape[0] == len(data)
+    return design_matrix, info
 
--- test_house_prices.py
+++ test_house_prices.py
@@ -4,15 +4,25 @@ from house_prices import embed_column
 
 
 def test_embed_column():
-    repeated_codes = np.arange(10)
-    already_encoded = np.repeat(repeated_codes, 3)
-    encoded, mapping = embed_column(already_encoded)
+    n_unique = 7
+    n_repeats = 3
+    offset = 5
+    repeated_codes = np.arange(n_unique)
+    already_encoded = np.repeat(repeated_codes, n_repeats)
+    encoded, mapping = embed_column(already_encoded + offset, add_unkown_token=False)
     assert np.all(already_encoded == encoded)
-    assert np.all(mapping == repeated_codes)
+    assert len(mapping) == n_unique
+    _, mapping_unk = embed_column(already_encoded, add_unkown_token=True)
+    assert mapping_unk["UNK_TOKEN"] == n_unique
 
     np.random.seed(15)
-    all_unique = np.random.randn(100)
-    encoded, mapping = embed_column(all_unique)
+    n_unique = 100
+    all_unique = np.random.randn(n_unique)
+    encoded, mapping = embed_column(all_unique, add_unkown_token=False)
     to_sorted = np.argsort(encoded)
     assert np.all(encoded[to_sorted] == np.arange(all_unique.shape[0]))
-    assert np.allclose(mapping, all_unique[to_sorted])
+    assert np.allclose(sorted(list(mapping.keys()), key=lambda x: mapping[x]), all_unique[to_sorted])
+    assert "UNK_TOKEN" not in mapping
+    _, mapping_unk = embed_column(all_unique, add_unkown_token=True)
+    assert mapping_unk["UNK_TOKEN"] == n_unique
+
